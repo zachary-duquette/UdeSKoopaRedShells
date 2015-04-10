2231470cbaedc9aeef1a3f2b22a2b96efcecb6c6
Julien <Julien.Meunier@usherbrooke.ca>
2015-04-10 12:02:23 -0400

    Uncommented the call to GameServer.......... And cleaned some code, change the reading method to read exactly 100 bytes

--- RedShells/RedShells/GameServer.cpp
+++ RedShells/RedShells/GameServer.cpp
@@ -18,7 +18,7 @@ GameServer::~GameServer()
 
 void GameServer::Update()
 {
-	while (true )//!Game::Get()->IsGameFinished())
+	while (true)//!Game::Get()->IsGameFinished())
 	{
 		for (int player = 0; player < m_inGameSockets.size(); ++player)
 		{
@@ -26,11 +26,10 @@ void GameServer::Update()
 			{
 				if (m_inGameSockets[player].first.mi_isAlive)
 				{
-					char message[100]{};
-					m_inGameSockets[player].second.receive(boost::asio::buffer(&message, 100));
-					std::string id{ message, 1};
+					char message[100];
+					boost::asio::read(m_inGameSockets[player].second, boost::asio::buffer(&message, 100));
+					std::string id{ message, 1 };
 					char* payload = message;
-					//std::string payload{ p + 3 };
 					if (id == "A") // Avg
 					{
 						float value = std::stof(payload + 1);
@@ -69,6 +68,13 @@ void GameServer::Update()
 				m_inGameSockets[player].second.close();
 				Update();
 			}
+			catch (...)
+			{
+				m_inGameSockets[player].first.mi_isAlive = false;
+				m_inGameSockets[player].second.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+				m_inGameSockets[player].second.close();
+				Update();
+			}
 		}
 	}
 }
@@ -78,7 +84,7 @@ void GameServer::AcceptSocket(tcp::socket& socket)
 	char answer[100]{};
 	Game* game = Game::Get();
 
-	if (true )// game->CanPlayersJoin())
+	if (true)// game->CanPlayersJoin())
 	{
 		auto controller = new CellphoneController;
 		int playerId = game->AddPlayer(controller);
@@ -112,7 +118,6 @@ void GameServer::DenySocket(tcp::socket& socket)
 
 void GameServer::Listen()
 {
-	//stop condition...
 	boost::asio::io_service io_service;
 	tcp::acceptor acceptor(io_service, tcp::endpoint(boost::asio::ip::address::from_string("192.168.0.100"), 8779));
 	for (;;)
@@ -121,28 +126,16 @@ void GameServer::Listen()
 		try
 		{
 			acceptor.accept(newSocket);
-			//char message[100] {};
-			//newSocket.receive(boost::asio::buffer(&message, 100));
-			//std::string id{ message, 3 };
-			//char* p = message;
-			//std::string payload{ p + 3 };
 
-			//if (id == "Joi")
-			//{
-			//	if (!Game::Get()->IsGameFull())
-			//	{
-			AcceptSocket(newSocket);
-//				}
-	//			else
-		//		{
-		//			DenySocket(newSocket);
-		//		}
-	//		}
-	//		else
-	//		{
-	//			newSocket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-	//			newSocket.close();
-	//		}
+			/*if (!Game::Get()->IsGameFull())
+			{*/
+				AcceptSocket(newSocket);
+			/*}
+			else
+			{
+				DenySocket(newSocket);
+			}*/
+
 		}
 		catch (boost::system::error_code ec)
 		{
@@ -153,60 +146,3 @@ void GameServer::Listen()
 		}
 	}
 }
-
-
-//std::string GameServer::WriteJson(std::string id, std::string value)
-//{
-//	boost::property_tree::ptree pt{};
-//	pt.put(id, value);
-//	std::ostringstream buf{};
-//	boost::property_tree::write_json(buf, pt, false);
-//	return buf.str();
-//}
-//
-//boost::property_tree::ptree GameServer::ReadJson(std::string json)
-//{
-//	boost::property_tree::ptree pt2{};
-//	std::istringstream is{ json };
-//	boost::property_tree::read_json(is, pt2);
-//	return pt2;
-//}
-
-//
-//void GameServer::NewConnection()
-//{
-//	try
-//	{
-//		char message[100] {};
-//		m_newSocket->receive(boost::asio::buffer(&message, 100));
-//		auto json = ReadJson(std::string{ message });
-//
-//		if (json.back().first == "")
-//		{
-//			if (true /*GameState.getNumPlayers() <= GameState.getMaxNumPlayers*/)
-//			{
-//				AcceptSocket();
-//			}
-//			else
-//			{
-//				DenySocket();
-//			}
-//		}
-//		else
-//		{
-//			m_newSocket->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-//			m_newSocket->close();
-//		}
-//
-//		*m_newSocket = tcp::socket{ m_ioService };
-//		m_acceptor->async_accept(*m_newSocket, boost::bind(&GameServer::NewConnection, this));
-//	}
-//	catch (boost::system::error_code ec)
-//	{
-//		m_newSocket->shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
-//		m_newSocket->close();
-//		std::cerr << ec.message() << std::endl;
-//		*m_newSocket = tcp::socket{ m_ioService };
-//		m_acceptor->async_accept(*m_newSocket, boost::bind(&GameServer::NewConnection, this));
-//	}
-//}
